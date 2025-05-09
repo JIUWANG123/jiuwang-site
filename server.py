5cc71f78cef3eff477ff1d9a061047b98d4bacd8
JIUWANG123 <127936821+JIUWANG123@users.noreply.github.com>
2025-05-10 00:49:07 +0800

    Add files via upload

--- server.py
+++ server.py
@@ -1,46 +1,4 @@
+from upload import app
 
-import os
-from flask import Flask, request, send_from_directory, jsonify
-from werkzeug.utils import secure_filename
-
-app = Flask(__name__)
-UPLOAD_FOLDER = os.path.join('images', 'uploads')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-UPLOAD_PASSWORD = "852"  # 自定义上传密码
-
-os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-
-@app.route('/upload', methods=['POST'])
-def upload_file():
-    password = request.form.get("secret")
-    if password != UPLOAD_PASSWORD:
-        return '权限不足', 403
-
-    if 'file' not in request.files:
-        return 'No file part', 400
-    file = request.files['file']
-    if file.filename == '':
-        return 'No selected file', 400
-    filename = secure_filename(file.filename)
-    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-    return 'OK', 200
-
-@app.route('/list')
-def list_files():
-    files = os.listdir(app.config['UPLOAD_FOLDER'])
-    return jsonify(files)
-
-@app.route('/images/uploads/<path:filename>')
-def serve_image(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-@app.route('/')
-def index():
-    return send_from_directory('.', 'index.html')
-
-@app.route('/<path:path>')
-def static_proxy(path):
-    return send_from_directory('.', path)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
